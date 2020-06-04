2a90ee6205533f64bc834489d143a80e39280080
Rittin Shingari <61085942+rittin97@users.noreply.github.com>
2020-06-03 23:29:55 -0400

    Add files via upload

---
+++ Tic Tac Toe/TicTacToe.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace Tic_Tac_Toe
+{
+    partial class TicTacToe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTurn = new System.Windows.Forms.Label();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTurn
+            // 
+            this.lblTurn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblTurn.BackColor = System.Drawing.Color.Green;
+            this.lblTurn.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTurn.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.lblTurn.Location = new System.Drawing.Point(42, 9);
+            this.lblTurn.Name = "lblTurn";
+            this.lblTurn.Size = new System.Drawing.Size(198, 45);
+            this.lblTurn.TabIndex = 1;
+            this.lblTurn.Text = "Your Turn!";
+            this.lblTurn.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btn3
+            // 
+            this.btn3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3.Location = new System.Drawing.Point(178, 68);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(62, 60);
+            this.btn3.TabIndex = 6;
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.Location = new System.Drawing.Point(110, 68);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(62, 60);
+            this.btn2.TabIndex = 5;
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.Location = new System.Drawing.Point(42, 68);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(62, 60);
+            this.btn1.TabIndex = 4;
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn9.Location = new System.Drawing.Point(178, 200);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(62, 60);
+            this.btn9.TabIndex = 15;
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn8.Location = new System.Drawing.Point(110, 200);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(62, 60);
+            this.btn8.TabIndex = 14;
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn7.Location = new System.Drawing.Point(42, 200);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(62, 60);
+            this.btn7.TabIndex = 13;
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn6.Location = new System.Drawing.Point(178, 134);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(62, 60);
+            this.btn6.TabIndex = 12;
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn5.Location = new System.Drawing.Point(110, 134);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(62, 60);
+            this.btn5.TabIndex = 11;
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn4.Location = new System.Drawing.Point(42, 134);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(62, 60);
+            this.btn4.TabIndex = 10;
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // TicTacToe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 299);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.lblTurn);
+            this.Name = "TicTacToe";
+            this.Text = "Game";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.TicTacToe_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTurn;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn4;
+    }
+}
---
+++ Tic Tac Toe/TicTacToe.cs
@@ -0,0 +1,477 @@
+﻿/*
+ * Name: Rittin Shingari
+ * Course: PRG655 NAA
+ * Project: Tic Tac Toe
+ * Due Date: 10 April 2020
+ * Professor: Goran Svenk
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net.Sockets;
+using System.Media;
+namespace Tic_Tac_Toe
+{
+    public partial class TicTacToe : Form
+    {
+        private char PlayerChar;
+        private char OpponentChar;
+        private Socket sock;
+        private BackgroundWorker Receiver = new BackgroundWorker();
+        private TcpListener server = null;
+        private TcpClient client;
+        public bool endGame = false;
+
+        private void playSound()
+        {
+            SoundPlayer splayer = new SoundPlayer(@"sound.wav");
+            splayer.Play();
+        }
+
+        private void finalSound()
+        {
+            SoundPlayer splayer = new SoundPlayer(@"final.wav");
+            splayer.Play();
+        }
+        public TicTacToe(bool isHost, string ip = null)
+        {
+            InitializeComponent();
+            Receiver.DoWork += Receiver_Work;
+            CheckForIllegalCrossThreadCalls = false;
+            if (isHost)
+            {
+                PlayerChar = 'X';
+                OpponentChar = 'O';
+                server = new TcpListener(System.Net.IPAddress.Any, 4545);
+                server.Start();
+                sock = server.AcceptSocket();
+            }
+
+            else
+            {
+                PlayerChar = 'O';
+                OpponentChar = 'X';
+                try
+                {
+                    client = new TcpClient(ip, 4545);
+                    sock = client.Client;
+                    Receiver.RunWorkerAsync();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    Close();
+                }
+            }
+        }
+
+        private void Receiver_Work(object sender, DoWorkEventArgs e)
+        {
+            if (CurrentState())
+                return;
+            FreezeButtons();
+            lblTurn.BackColor = Color.Red;
+            lblTurn.Text = "Opponent's Turn!";
+            Turn();
+            lblTurn.BackColor = Color.Green;
+            lblTurn.Text = "Your Turn!";
+            if (!CurrentState())
+                UnfreezeButtons();
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            playSound();
+            btn1.BackColor = Color.Green;
+            try
+            {
+                Thread t1 = new Thread(() =>
+                {
+                    byte[] number = { 1 };
+                    sock.Send(number);
+                    btn1.Text = PlayerChar.ToString();
+                    Receiver.RunWorkerAsync();
+                    Thread.Sleep(1000);
+                });
+                t1.Start();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            
+        }
+
+        private void Turn()
+        {
+            byte[] buffer = new byte[1];
+            sock.Receive(buffer);
+            if (buffer[0] == 1)
+                btn1.Text = OpponentChar.ToString();
+            if (buffer[0] == 2)
+                btn2.Text = OpponentChar.ToString();
+            if (buffer[0] == 3)
+                btn3.Text = OpponentChar.ToString();
+            if (buffer[0] == 4)
+                btn4.Text = OpponentChar.ToString();
+            if (buffer[0] == 5)
+                btn5.Text = OpponentChar.ToString();
+            if (buffer[0] == 6)
+                btn6.Text = OpponentChar.ToString();
+            if (buffer[0] == 7)
+                btn7.Text = OpponentChar.ToString();
+            if (buffer[0] == 8)
+                btn8.Text = OpponentChar.ToString();
+            if (buffer[0] == 9)
+                btn9.Text = OpponentChar.ToString();
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            playSound();
+            btn2.BackColor = Color.Green;
+            try
+            {
+                Thread t2 = new Thread(() =>
+                {
+                    byte[] number = { 2 };
+                    sock.Send(number);
+                    btn2.Text = PlayerChar.ToString();
+                    Receiver.RunWorkerAsync();
+                    Thread.Sleep(1000);
+                });
+                t2.Start();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            playSound();
+            btn3.BackColor = Color.Green;
+            try
+            {
+                Thread t3 = new Thread(() =>
+                {
+                    byte[] number = { 3 };
+                    sock.Send(number);
+                    btn3.Text = PlayerChar.ToString();
+                    Receiver.RunWorkerAsync();
+                    Thread.Sleep(1000);
+                });
+                t3.Start();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            playSound();
+            btn4.BackColor = Color.Green;
+            try
+            {
+                Thread t4 = new Thread(() =>
+                {
+                    byte[] number = { 4 };
+                    sock.Send(number);
+                    btn4.Text = PlayerChar.ToString();
+                    Receiver.RunWorkerAsync();
+                    Thread.Sleep(1000);
+                });
+                t4.Start();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            playSound();
+            btn5.BackColor = Color.Green;
+            try
+            {
+                Thread t5 = new Thread(() =>
+                {
+                    byte[] number = { 5 };
+                    sock.Send(number);
+                    btn5.Text = PlayerChar.ToString();
+                    Receiver.RunWorkerAsync();
+                    Thread.Sleep(1000);
+                });
+                t5.Start();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            playSound();
+            btn6.BackColor = Color.Green;
+            try
+            {
+                Thread t6 = new Thread(() =>
+                {
+                    byte[] number = { 6 };
+                    sock.Send(number);
+                    btn6.Text = PlayerChar.ToString();
+                    Receiver.RunWorkerAsync();
+                    Thread.Sleep(1000);
+                });
+                t6.Start();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            playSound();
+            btn7.BackColor = Color.Green;
+            try
+            {
+                Thread t7 = new Thread(() =>
+                {
+                    byte[] number = { 7 };
+                    sock.Send(number);
+                    btn7.Text = PlayerChar.ToString();
+                    Receiver.RunWorkerAsync();
+                    Thread.Sleep(1000);
+                });
+                t7.Start();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            playSound();
+            btn8.BackColor = Color.Green;
+            try
+            {
+                Thread t8 = new Thread(() =>
+                {
+                    byte[] number = { 8 };
+                    sock.Send(number);
+                    btn8.Text = PlayerChar.ToString();
+                    Receiver.RunWorkerAsync();
+                    Thread.Sleep(1000);
+                });
+                t8.Start();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            playSound();
+            btn9.BackColor = Color.Green;
+            try
+            {
+                Thread t9 = new Thread(() =>
+                {
+                    byte[] number = { 9 };
+                    sock.Send(number);
+                    btn9.Text = PlayerChar.ToString();
+                    Receiver.RunWorkerAsync();
+                    Thread.Sleep(1000);
+                });
+                t9.Start();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        private void FreezeButtons()
+        {
+            btn1.Enabled = false;
+            btn2.Enabled = false;
+            btn3.Enabled = false;
+            btn4.Enabled = false;
+            btn5.Enabled = false;
+            btn6.Enabled = false;
+            btn7.Enabled = false;
+            btn8.Enabled = false;
+            btn9.Enabled = false;
+        }
+
+        private void UnfreezeButtons()
+        {
+            if (btn1.Text == "")
+                btn1.Enabled = true;
+            if (btn2.Text == "")
+                btn2.Enabled = true;
+            if (btn3.Text == "")
+                btn3.Enabled = true;
+            if (btn4.Text == "")
+                btn4.Enabled = true;
+            if (btn5.Text == "")
+                btn5.Enabled = true;
+            if (btn6.Text == "")
+                btn6.Enabled = true;
+            if (btn7.Text == "")
+                btn7.Enabled = true;
+            if (btn8.Text == "")
+                btn8.Enabled = true;
+            if (btn9.Text == "")
+                btn9.Enabled = true;
+        }
+
+        private void youWon()
+        {
+            finalSound();
+            lblTurn.BackColor = Color.Green;
+            lblTurn.Text = "You Won!";
+            FreezeButtons();
+            Thread.Sleep(15000);
+            endGame = true;
+            Application.Exit();
+        }
+
+        private void youLost()
+        {
+            finalSound();
+            lblTurn.BackColor = Color.Red;
+            lblTurn.Text = "You Lost!";
+            FreezeButtons();
+            Thread.Sleep(15000);
+            endGame = true;
+            Application.Exit();
+        }
+
+        private void DrawMatch()
+        {
+            finalSound();
+            lblTurn.BackColor = Color.LightSalmon;
+            lblTurn.Text = "Draw Match!";
+            FreezeButtons();
+            Thread.Sleep(15000);
+            endGame = true;
+            Application.Exit();
+        }
+
+        private bool CurrentState()
+        {
+            //Horizontals
+            if (btn1.Text == btn2.Text && btn2.Text == btn3.Text && btn3.Text != "")
+            {
+                if (btn1.Text[0] == PlayerChar)
+                    youWon();
+                else
+                    youLost();
+                return true;
+            }
+
+            else if (btn4.Text == btn5.Text && btn5.Text == btn6.Text && btn6.Text != "")
+            {
+                if (btn4.Text[0] == PlayerChar)
+                    youWon();
+                else
+                    youLost();
+                return true;
+            }
+
+            else if (btn7.Text == btn8.Text && btn8.Text == btn9.Text && btn9.Text != "")
+            {
+                if (btn7.Text[0] == PlayerChar)
+                    youWon();
+                else
+                    youLost();
+                return true;
+            }
+
+            //Verticals
+            else if (btn1.Text == btn4.Text && btn4.Text == btn7.Text && btn7.Text != "")
+            {
+                if (btn1.Text[0] == PlayerChar)
+                    youWon();
+                else
+                    youLost();
+                return true;
+            }
+
+            else if (btn2.Text == btn5.Text && btn5.Text == btn8.Text && btn8.Text != "")
+            {
+                if (btn2.Text[0] == PlayerChar)
+                    youWon();
+                else
+                    youLost();
+                return true;
+            }
+
+            else if (btn3.Text == btn6.Text && btn6.Text == btn9.Text && btn9.Text != "")
+            {
+                if (btn3.Text[0] == PlayerChar)
+                    youWon();
+                else
+                    youLost();
+                return true;
+            }
+
+            else if (btn1.Text == btn5.Text && btn5.Text == btn9.Text && btn9.Text != "")
+            {
+                if (btn1.Text[0] == PlayerChar)
+                    youWon();
+                else
+                    youLost();
+                return true;
+            }
+
+            else if (btn3.Text == btn5.Text && btn5.Text == btn7.Text && btn7.Text != "")
+            {
+                if (btn3.Text[0] == PlayerChar)
+                    youWon();
+                else
+                    youLost();
+                return true;
+            }
+
+            //Draw
+            else if (btn1.Text != "" && btn2.Text != "" && btn3.Text != "" && btn4.Text != "" && btn5.Text != "" && btn6.Text != "" && btn7.Text != "" && btn8.Text != "" && btn9.Text != "")
+            {
+                DrawMatch();
+                return true;
+            }
+            return false;
+        }
+
+        private void TicTacToe_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Receiver.WorkerSupportsCancellation = true;
+            Receiver.CancelAsync();
+            if (server != null)
+                server.Stop();
+            this.Hide();
+            this.Parent = null;
+            e.Cancel = true;
+        }
+    }
+}
